8cfb0aabce54312d75374600fff6ec0a82707762
shubham kalamkar <shubhamkalamkar0101@gmail.com>
2025-03-22 14:30:51 +0530

    added stop btn and add pluging for service start and stop

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.shubham.app',
-  appName: 'jio location',
+  appName: 'Gio location',
   webDir: 'dist/JioLocation/browser'
 };
 
--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -4,6 +4,7 @@ import { LocationAccuracy } from '@awesome-cordova-plugins/location-accuracy/ngx
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from '../../services/auth.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Location } from 'location';
 import {
   AndroidSettings,
   IOSSettings,
@@ -26,6 +27,9 @@ export class LoginComponent implements OnInit {
   lon!: number;
   address!: string;
   isLoading = false;
+  serviceStatus: boolean = false;
+  isButtonDisabled = false;
+  isRefreshDisabled = false;
 
   constructor(
     private auth: AuthService,
@@ -33,15 +37,15 @@ export class LoginComponent implements OnInit {
     private toastr: ToastrService
   ) {}
 
-  ngOnInit(): void {
-     // Check Notification Permission on Init
+  async ngOnInit(): Promise<void> {
+    this.getServiceStatus();
   }
 
   /**
    * 1️⃣ Check Notification Permission
    */
   async checkNotificationPermission() {
-    console.log("checking permission")
+    console.log('checking permission');
     const status = await PushNotifications.checkPermissions();
     console.log('Notification permission:', status);
 
@@ -61,8 +65,9 @@ export class LoginComponent implements OnInit {
   /**
    * 2️⃣ Check Location Permission and Fetch Location
    */
-  getCurrentLocation = async () => {
+  getCurrentLocation = async (): Promise<boolean> => {
     this.isLoading = true; // Show spinner
+    this.isRefreshDisabled = true;
 
     try {
       const permissionStatus = await Geolocation.checkPermissions();
@@ -74,7 +79,7 @@ export class LoginComponent implements OnInit {
         if (requestStatus.location !== 'granted') {
           await this.openSettings(true); // Open settings if permission is denied
           this.isLoading = false;
-          return;
+          return false;
         }
       }
 
@@ -88,37 +93,88 @@ export class LoginComponent implements OnInit {
         enableHighAccuracy: true,
       };
 
-      const position = await Geolocation.getCurrentPosition(options);
-      this.lat = position.coords.latitude;
-      this.lon = position.coords.longitude;
+      // const position = await Geolocation.getCurrentPosition(options);
+      // this.lat = position.coords.latitude;
+      // this.lon = position.coords.longitude;
 
-      this.auth.getAddress(this.lat, this.lon).subscribe({
-        next: (data) => {
-          console.log('Address:', data);
-          this.address = data.display_name;
-          this.isLoading = false;
-        },
-        error: (err) => {
-          console.log('Error fetching address:', err);
-          this.toastr.error('Failed to fetch address', 'Error');
-          this.isLoading = false;
-        },
-      });
+      // this.auth.getAddress(this.lat, this.lon).subscribe({
+      //   next: (data) => {
+      //     console.log('Address:', data);
+      //     this.address = data.display_name;
+      //     this.isLoading = false;
+      //   },
+      //   error: (err) => {
+      //     console.log('Error fetching address:', err);
+      //     this.toastr.error('Failed to fetch address', 'Error');
+      //     this.isLoading = false;
+      //   },
+      // });
+      const result1 = await Location.getLocation();
+      this.lat = result1.latitude;
+      this.lon = result1.longitude;
+      this.address = result1.address;
+      this.isLoading = false;
+      await this.getServiceStatus();
+      this.isRefreshDisabled = false;
+      console.log('get address=>>', result1);
+      return true;
     } catch (e: any) {
       console.log('Location error:', e);
+      this.isRefreshDisabled = false;
       if (e?.message === 'Location services are not enabled') {
         await this.openSettings();
       }
       this.toastr.error('Failed to fetch location', 'Error');
       this.isLoading = false;
+      return false;
     }
   };
 
+  async startService() {
+    try {
+      this.isButtonDisabled = true;
+      let valid = await this.getCurrentLocation();
+      if (valid) {
+        const result = await Location.startBackgroundService();
+        await this.getServiceStatus();
+        this.isButtonDisabled = false;
+        console.log(result.message);
+      }
+    } catch (error) {
+      this.isButtonDisabled = false;
+      console.error('Error starting background service:', error);
+    }
+  }
+
+  async stopService() {
+    try {
+      this.isButtonDisabled = true;
+      const result = await Location.stopBackgroundService();
+      await this.getServiceStatus();
+      this.isButtonDisabled = false;
+      console.log(result.message);
+    } catch (error) {
+      this.isButtonDisabled = false;
+      console.error('Error starting background service:', error);
+    }
+  }
+
+  async getServiceStatus() {
+    try {
+      const res = await Location.checkServiceStatus();
+      this.serviceStatus = res.isRunning;
+      console.log('result', this.serviceStatus);
+    } catch (error) {
+      console.error('Error starting background service:', error);
+    }
+  }
   /**
    * 3️⃣ Open Native Settings (Location or App Settings)
    */
   openSettings = (app = false) => {
     console.log('Opening settings...');
+    this.isRefreshDisabled = false;
+    this.isButtonDisabled = false;
     return NativeSettings.open({
       optionAndroid: app
         ? AndroidSettings.ApplicationDetails
@@ -132,6 +188,8 @@ export class LoginComponent implements OnInit {
    */
   async enableGps() {
     const canRequest = await this.locationAccuracy.canRequest();
+    this.isRefreshDisabled = false;
+    this.isButtonDisabled = false;
     if (canRequest) {
       await this.locationAccuracy.request(
         this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY
